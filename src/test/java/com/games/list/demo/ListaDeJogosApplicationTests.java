ccb30163ec56cd57e2e43f284e32b4bb3a640a6f
Salomao Dias <salomaodiasdiniz22@gmail.com>
2023-11-15 20:19:50 -0300

    Criado entidade Games

---
+++ src/test/java/com/games/list/demo/ListaDeJogosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.games.list.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaDeJogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
